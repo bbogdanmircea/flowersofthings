25477cd83e025b9e3914172685200cd40810ad29
Bogdan Bodnarescu <bbogdanmircea@gmail.com>
2019-12-08 11:32:48 +0100

    Save data.csv as it is too big: -data.csv saved -modified humidity_timeseries.sh -plot_humidity.py

--- Debug/humidity_timeseries.sh
+++ Debug/humidity_timeseries.sh
@@ -1,8 +1,8 @@
 while true; do
   timestamp=$(date +%s)
-  humidity=$(curl --request GET --url http://YOUR-IP-or-DNS/soil)
-  hum=$(curl --request GET --url http://YOUR-IP-or-DNS/hum)
-  temp=$(curl --request GET --url http://YOUR-IP-or-DNS/temp)
-  echo "$timestamp\t$humidity\t$hum\t$temp"  >> ./soil_data.txt
+  humidity=$(curl --request GET --url esp32.local/soil)
+  hum=$(curl --request GET --url esp32.local/hum)
+  temp=$(curl --request GET --url esp32.local/temp)
+  echo "$timestamp\t$humidity\t$hum\t$temp"  >> ./data.txt
   sleep 300
 done
